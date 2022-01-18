70999a0c2982cfb4ab5a2ab87cf5e160fd6935d6
jrenzzo <u202118568@upc.edu.pe>
2022-01-18 15:51:55 -0500

    update

--- script.js
+++ script.js
@@ -1,38 +1,18 @@
 const container = document.querySelector('.container')
 
-/*
-const content = document.createElement('div')
-content.textContent = "First div"
-content.setAttribute('style', 'background-color: gray')
-
-container.appendChild(content)
-
-*/
-function makeGrid(){
-    for(let i=0; i<16;i++){
-        let row = document.createElement('div');
-        row.classList.add('row')
-        for(let j=0; j<16; j++){
-            var box = document.createElement('div');
-            box.classList.add('box')
-            row.appendChild(box);
-        }
-        container.appendChild(row);
+function makeGrid(size){
+    for(let i=0; i<size * size;i++){
+        const box = document.createElement('div')
+        box.classList.add('box')
+        container.appendChild(box)
     }
 }
-makeGrid()
+
+makeGrid(26)
 
 const boxes = document.querySelectorAll('.box')
 
 boxes.forEach(box => box.addEventListener('mouseover', ()=> {
     box.classList.add('painted-box')
-
 }))
 
-
-
-/*
-function onHover(e) {
-     //console.log(e.target)   
-  }
-*/
